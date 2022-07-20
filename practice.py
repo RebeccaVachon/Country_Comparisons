ac6266a67dcc2cac1ed157990be2f0402cd9454e
RebeccaVachon <105834731+RebeccaVachon@users.noreply.github.com>
2022-07-20 19:11:17 -0400

    Delete practice.py

--- practice.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-comparison_df = pd.read_csv('County_Comparison.csv')
-print(comparison_df.dtypes)
-
-excel_comparison_df = pd.read_excel('County_Comparison.xlsx', sheet_name='County_Comparison')
-print(excel_comparison_df.dtypes)
